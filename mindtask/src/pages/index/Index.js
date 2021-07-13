e2949d89a4617bc86c4b28ecea477f45ad741b51
Xinyao Niu <morryniu123@gmail.com>
2021-07-13 22:21:22 +1000

    bring svg and hardcode padding

--- mindtask/src/pages/index/Index.js
+++ mindtask/src/pages/index/Index.js
@@ -1,14 +1,18 @@
 import React from "react";
+import logo from "./corner.svg";
 
 class Index extends React.Component {
   render () {
-    return(    
-      <div className="index">
-        <ul>
-          <li>Instagram</li>
-          <li>WhatsApp</li>
-          <li>Oculus</li>
-        </ul>
+    return(
+      <div style={{flex:1}}>
+        <div style={{flex:1, textAlign:'center', paddingTop:"10%"}}>
+          <text style={{fontSize:"80px"}}>
+            Plan Your Projects
+          </text>
+        </div>
+        <div style={{paddingTop:"15%"}}>
+          <img src={logo} alt="logo"/>
+        </div>
       </div>
     );
   }
