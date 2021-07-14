341e7829d29dadaa8c9f35eeec9ea525be153590
Alun Xu <Alun-XWL@outlook.com>
2021-07-14 16:47:39 +0800

    Feature: Nested Task Demonstration

--- mindtask/src/App.js
+++ mindtask/src/App.js
@@ -1,38 +1,42 @@
-import React from 'react';
-import './App.css';
-import NavBar from './components/nav_bar/NavBar'
+import React from "react";
+import "./App.css";
+import NavBar from "./components/nav_bar/NavBar";
 
-import PageNavigator from './pages/PageNavigator.js';
+import PageNavigator from "./pages/PageNavigator.js";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import EditPage from "./pages/edit/EditPage";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoggedIn: true,
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			isLoggedIn: true,
+		};
+	}
 
-  login = () => {
-    this.setState({
-      isLoggedIn: true,
-    })
-  }
+	login = () => {
+		this.setState({
+			isLoggedIn: true,
+		});
+	};
 
-  logout = () => {
-    this.setState({
-      isLoggedIn: false,
-    })
-  }
+	logout = () => {
+		this.setState({
+			isLoggedIn: false,
+		});
+	};
 
-  render() {
-    return (
-      <div>
-        <NavBar isLoggedIn={this.state.isLoggedIn} logout={this.logout}/>
-        <PageNavigator /> 
-      </div>
-      
-    );
-  }
+	render() {
+		return (
+			<BrowserRouter>
+				<NavBar
+					isLoggedIn={this.state.isLoggedIn}
+					logout={this.logout}
+				/>
+				<PageNavigator />
+			</BrowserRouter>
+		);
+	}
 }
 
 export default App;
--- mindtask/src/pages/PageNavigator.js
+++ mindtask/src/pages/PageNavigator.js
@@ -1,39 +1,38 @@
-import {React} from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { React } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { createBrowserHistory } from "history";
 
-import Home from './home/Home.js';
-import Index from './index/Index.js';
-import About from './about/About.js';
+import Home from "./home/Home.js";
+import Index from "./index/Index.js";
+import About from "./about/About.js";
+import EditPage from "./edit/EditPage.js";
 
 const customHistory = createBrowserHistory();
 
 const PageNavigator = () => {
-  return(
-    <div>
-      <Router history={customHistory}>
-        <Switch>
-          <Route exact path="/">
-            <Index/>
-          </Route>
-          <Route path="/index">
-            <Index/>
-          </Route>
-          <Route path="/home">
-            <Home/>
-          </Route>
-          <Route path="/about">
-            <About/>
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+	return (
+		<div>
+			<Router history={customHistory}>
+				<Switch>
+					<Route exact path="/">
+						<Index />
+					</Route>
+					<Route path="/index">
+						<Index />
+					</Route>
+					<Route path="/home">
+						<Home />
+					</Route>
+					<Route path="/about">
+						<About />
+					</Route>
+					<Route exact path="/editPage">
+						<EditPage />
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
 };
 
 export default PageNavigator;
-
